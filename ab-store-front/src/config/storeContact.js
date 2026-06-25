668c8ae68c485c80e03cd563e624c9665287d2ec
abderrahmaneknc <a.kennouche@esi-sba.dz>
2026-06-25 19:13:55 +0100

    update design

--- ab-store-front/src/config/storeContact.js
+++ ab-store-front/src/config/storeContact.js
@@ -1,7 +1,7 @@
 /** Update these with your real store contact details */
-export const STORE_PHONE = '0555123456';
-export const STORE_PHONE_DISPLAY = '0555 12 34 56';
-export const STORE_WHATSAPP = '213555123456';
+export const STORE_PHONE = '0557901966';
+export const STORE_PHONE_DISPLAY = '0557901966';
+export const STORE_WHATSAPP = '213557901966';
 
 export const phoneHref = `tel:+${STORE_WHATSAPP}`;
 export const whatsappHref = `https://wa.me/${STORE_WHATSAPP}`;
